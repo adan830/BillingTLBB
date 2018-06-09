bbfc1fd40a8b17e5bdb61432ba2a192f8a9d26ff
Dark.Hades <dark.hades.1102@gmail.com>
2018-06-09 17:59:01 +0900

    Session

--- include/billing/net/Session.hpp
+++ include/billing/net/Session.hpp
@@ -3,28 +3,21 @@
 
 #include <asio.hpp>
 
-#include "billing/net/Packet.hpp"
-
 namespace net
 {
   class Session
   {
     private:
       asio::ip::tcp::socket m_socket;
-      Packet m_packet;
 
     public:
-      Session(asio::ip::tcp::socket socket, const Packet& packet = Packet());
+      Session(asio::ip::tcp::socket socket);
 
     public:
       void start();
 
     public:
-      const asio::ip::tcp::socket* getSocket() const;
-      asio::ip::tcp::socket* getSocket();
-
-      const Packet& getPacket() const;
-      Packet& getPacket();
+      const asio::ip::tcp::socket& getSocket() const;
   };
 }
 
--- src/net/Session.cpp
+++ src/net/Session.cpp
@@ -7,20 +7,25 @@
 
 namespace net
 {
-  Session::Session(asio::ip::tcp::socket socket, const Packet& packet) :
-    m_socket(std::move(socket)),
-    m_packet(packet)
+  Session::Session(asio::ip::tcp::socket socket) :
+    m_socket(std::move(socket))
   {
   }
 
   void Session::start()
   {
+    Packet m_packet;
     m_socket.async_read_some(
       asio::buffer(m_packet.getBuffer()),
-      [this](const std::error_code ec, const std::size_t length)
+      [this, &m_packet](const std::error_code ec, const std::size_t length)
       {
         std::cout << "Received " << length << " byte(s) with Error Code: " << ec
         << std::endl;
+
+        if (!ec)
+        {
+          std::cout << m_packet.getBuffer().data() << std::endl;
+        }
       }
       );
   }
