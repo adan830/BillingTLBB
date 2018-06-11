3968125906c42dca72d07ec3bab432ca8a80115c
Dark.Hades <dark.hades.1102@gmail.com>
2018-06-11 14:15:18 +0900

    Build on windows specify

--- build/cmake_modules/Definitions.cmake
+++ build/cmake_modules/Definitions.cmake
@@ -1,2 +1,8 @@
 add_definitions(-DASIO_STANDALONE -DASIO_HAS_STD_CHRONO)
 
+# OS specify
+if (MSYS OR WIN32 OR MINGW OR CYGWIN)
+  add_definitions(-D__USE_W32_SOCKETS)
+  add_definitions(-D_WIN32_WINNT=0x0501)
+endif()
+
