325b0c249a7f20bdf093e4cd46ef6bd323bc94d3
Dark.Hades <dark.hades.1102@gmail.com>
2018-06-19 12:13:05 +0900

    Fix error linking wsock32

--- build/cmake_modules/LinkLibraries.cmake
+++ build/cmake_modules/LinkLibraries.cmake
@@ -1,17 +1,13 @@
-set(LIBS_REQUIRED ${LIBS_REQUIRED}
-  dl
-  pthread
-  )
-
 # OS specify
 if (MSYS OR WIN32 OR MINGW OR CYGWIN)
-  find_library(LIB_WS2_32 NAMES ws2_32)
-  if (NOT LIB_WS2_32)
-    message(FATAL_ERROR "Lib ws2_32 not found")
-  endif()
-  message(STATUS "Found winsock2_32 ${LIB_WS2_32}")
   set(LIBS_REQUIRED ${LIBS_REQUIRED}
-    ${LIB_WS2_32}
+    -lws2_32
+    -lwsock32
+    )
+else()
+  set(LIBS_REQUIRED ${LIBS_REQUIRED}
+    -lpthread
+    -ldl
     )
 endif()
 
