c8e4cdedf55da90b4eeec9a9e8aca5bed1602067
Dark.Hades <dark.hades.1102@gmail.com>
2018-06-22 07:32:38 +0000

    Compiler settings

--- build/cmake_modules/Definitions.cmake
+++ build/cmake_modules/Definitions.cmake
@@ -6,9 +6,3 @@ if (MSYS OR WIN32 OR MINGW OR CYGWIN)
   add_definitions(-D_WIN32_WINNT=0x0501)
 endif()
 
-# Compile Mode
-if (NOT (CMAKE_BUILD_TYPE STREQUAL Release))
-  add_definitions(-DBILLING_DEBUG)
-endif()
-message(STATUS "Compiling in ${CMAKE_BUILD_TYPE} Mode")
-
