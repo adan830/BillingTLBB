17501d490d738bb86100d2edd102bd50487e6e47
Dark.Hades <dark.hades.1102@gmail.com>
2018-07-07 22:02:13 +0900

    MAX_SESSION

--- build/cmake_modules/Definitions.cmake
+++ build/cmake_modules/Definitions.cmake
@@ -6,3 +6,7 @@ if (MSYS OR WIN32 OR MINGW OR CYGWIN)
   add_definitions(-D_WIN32_WINNT=0x0501)
 endif()
 
+if (BILLING_MAX_SESSION)
+  add_definitions(-D__BILLING_MAX_SESSION__=${BILLING_MAX_SESSION})
+endif()
+
